80114414f1d510e432b0298a8b1981b85beddeb2
Everette <37136301+Digx7@users.noreply.github.com>
2026-03-27 13:26:18 -0700

    Got MVP script working

--- diskArrayTest.sh
+++ diskArrayTest.sh
@@ -3,59 +3,104 @@
 
 echo "Begining script $0"
 
+# Getting Disk List =======================
+
 echo "Getting Disk List"
 diskutil list -plist > diskList.xml
 echo "Created Disk List"
 
-echo "Getting Volume List"
-echo "<root>" > "volumeList.xml"
-xpath -e "//dict[key/text()='VolumeUUID']" "diskList.xml" >> "volumeList.xml"
-echo "</root>" >> "volumeList.xml"
-echo "created Volume List"
-
-echo "Getting Volume UUIDs"
-xpath -e "/root/dict/string[last()]/text()" "volumeList.xml" > "volumeUUIDList.txt"
-echo "Created Volume UUIDs List"
-
 echo "Getting Disk Names"
-xpath -e "//array[last()]/string/text()" "diskList.xml" > "diskNameList.txt"
+xpath -q -e "//array[last()]/string/text()" "diskList.xml" > "diskNameList.txt"
 
 echo "Creating Array"
 
-my_disk_array=()
+initial_disk_array=()
+filtered_disk_array=()
 
 while IFS= read -r line; do
-        my_disk_array+=("$line")
+        initial_disk_array+=("$line")
 done < "diskNameList.txt"
 
 disk_array_length=${#my_disk_array[@]}
 echo "Disk Array length: $disk_array_length"
 
-my_volume_array=()
+echo "Looping through initial disk array"
+for disk in "${initial_disk_array[@]}"; do
+	echo "Disk: $disk"
+	if [[ "$disk" != "disk"[0-1]* ]]; then
+		echo "Should add disk to filter"
+		filtered_disk_array+=("$disk")
+	fi
+done
+echo "Finished looping through initial disk array"
 
-while IFS= read -r line; do
-	my_volume_array+=("$line")
-done < "volumeUUIDList.txt"
+echo "Looping through filtered disk array"
+for disk in "${filtered_disk_array[@]}"; do
+	echo "Filtered Disk: $disk"
+done
+echo "Finished looping through filtered disk array"
 
-volume_array_length=${#my_volume_array[@]}
-echo "Volume Array length: $volume_array_length"
+# Getting Volume List =========================
 
+volume_UUID_array=()
 
-echo "Looping through disk array"
+echo "Getting volume UUIDs from filtered disk array"
+for disk in "${filtered_disk_array[@]}"; do
+	echo "Filtered Disk: $disk"
 
-for item in "${my_disk_array[@]}"; do
-        echo "Disk: $item"
+	echo "Creating disk list for $disk"
+	diskutil list -plist "$disk" > "${disk}List.xml"
+	echo "Created disk list for $disk"
+
+	echo "Creating volume list for $disk"
+	echo "<root>" > "${disk}volumeList.xml"
+	xpath -q -e "//dict[key/text()='VolumeUUID']" "${disk}List.xml" >> "${disk}volumeList.xml"
+	echo "</root>" >> "${disk}volumeList.xml"
+	echo "Created volume list for $disk"
+
+	echo "Creating volume UUID list for $disk"
+	xpath -q -e "/root/dict/string[last()]/text()" "${disk}volumeList.xml" > "${disk}volumeUUIDList.txt"
+	echo "Created volume UUID list for $disk"
+
+	echo "Adding volume UUIDs from $disk to array"
+	while IFS= read -r line; do
+		volume_UUID_array+=("$line")
+	done < "${disk}volumeUUIDList.txt"
+	echo "Added volume UUIDs from $disk to array"
+
+done
+echo "Got volume UUIDs from filtered disk array"
+
+for volume in "${volume_UUID_array[@]}"; do
+	echo "Volume: $volume"
 done
 
-echo "Finished Looping throug disk Array"
+# Erasing Volumes ======================
 
-
-echo "Looping through volume array"
-
-for item in "${my_volume_array[@]}"; do
-	echo "Volume UUID: $item"
+echo "Erasing all volumes"
+for volume in "${volume_UUID_array[@]}"; do
+	echo "Eraseing volume $volume"
+	diskutil eraseVolume MS-DOS iPod "$volume"
+	echo "Erased volume $volume"
 done
+echo "Erased all volumes"
 
-echo "Finished Looping throug volume Array"
+# Ejecting Disks ======================
 
-echo "Exiting Script $0"
+echo "Ejecting all disks"
+echo "Starting eject 1"
+for disk in "${filtered_disk_array[@]}"; do
+	diskutil eject "$disk"
+done
+echo "Finished eject 1"
+
+echo "Waiting for 1 minute for all disks to reconnect"
+sleep 60
+
+echo "Starting eject 2"
+for disk in "${filtered_disk_array[@]}"; do
+	diskutil eject "$disk"
+done
+echo "Finished eject 2"
+
+echo "All iPods should be restored now"
